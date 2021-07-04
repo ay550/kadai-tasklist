0c4cdfbde7213199c2406a28df1ec6e7440761f7
a.a <nyas.21212@gmail.com>
2021-07-04 18:01:09 +0900

    Add database files

---
+++ src/utils/DBUtil1.java
@@ -0,0 +1,25 @@
+//Hibernateに接続するためのおまじないDAO
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil1 {
+
+        private static final String PERSISTENCE_UNIT_NAME = "tasklist";
+        private static EntityManagerFactory emf;
+
+
+        public static EntityManager createEntityManager() {
+            return getEntityManagerFactory().createEntityManager();
+
+        }
+
+        private static EntityManagerFactory getEntityManagerFactory() {
+            if(emf == null) {
+                emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            }
+            return emf;
+        }
+}
